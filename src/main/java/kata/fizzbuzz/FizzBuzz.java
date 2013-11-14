a22ccbf4a1a4fe4e42be4f8b0ad936c54d20fe94
Juha Wiberg <juha.wiberg@gmail.com>
2013-11-14 18:38:36 +0200

    Test for "Fizz" printing passed.

--- src/main/java/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/FizzBuzz.java
@@ -3,10 +3,11 @@ package kata.fizzbuzz;
 public class FizzBuzz {
 	public String[] getArrayAnswer(int lastNumber) {
 		String[] answer = new String[lastNumber];
-		if(lastNumber > 0) {
-			for(int i = 0; i < lastNumber; i++) {
-				answer[i] = Integer.toString(i+1);	
-			}
+		for (int i = 1; i <= lastNumber; i++) {
+			if (i % 3 == 0)
+				answer[i - 1] = "Fizz";
+			else
+				answer[i - 1] = Integer.toString(i);
 		}
 		return answer;
 	}
--- src/test/java/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/kata/fizzbuzz/FizzBuzzTest.java
@@ -1,29 +1,34 @@
 package kata.fizzbuzz;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class FizzBuzzTest {
-	
+
 	private FizzBuzz fizzBuzz;
-	
+
 	@Before
 	public void before() {
 		this.fizzBuzz = new FizzBuzz();
 	}
-	
+
 	@Test
 	public void fizzBuzzNotNull() {
 		assertNotNull(fizzBuzz);
 	}
-	
+
 	@Test
 	public void testFizzBuzz() {
 		assertArrayEquals(new String[0], fizzBuzz.getArrayAnswer(0));
-		assertArrayEquals(new String[]{"1"}, fizzBuzz.getArrayAnswer(1));
-		assertArrayEquals(new String[]{"1","2"}, fizzBuzz.getArrayAnswer(2));
+		assertArrayEquals(new String[] { "1" }, fizzBuzz.getArrayAnswer(1));
+		assertArrayEquals(new String[] { "1", "2" }, fizzBuzz.getArrayAnswer(2));
+		assertArrayEquals(new String[] { "1", "2", "Fizz" },
+				fizzBuzz.getArrayAnswer(3));
+		assertArrayEquals(new String[] { "1", "2", "Fizz", "4" },
+				fizzBuzz.getArrayAnswer(4));
 	}
-	
+
 }
