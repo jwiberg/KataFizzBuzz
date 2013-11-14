b24e7a392023d4ae34b081aa887382ca86875281
Juha Wiberg <juha.wiberg@gmail.com>
2013-11-14 17:10:06 +0200

    Main class, and test class added. Default classes removed.

---
+++ src/main/java/kata/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,5 @@
+package kata.fizzbuzz;
+
+public class FizzBuzz {
+
+}
---
+++ src/test/java/kata/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package kata.fizzbuzz;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FizzBuzzTest {
+	
+	@Test
+	public void doesThisWork() {
+		assertNotNull(new FizzBuzz());
+	}
+	
+}
