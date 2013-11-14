551cd14e7add2a544567c4fb3b243c4c5b3c0cc4
Juha Wiberg <juha.wiberg@gmail.com>
2013-11-14 17:25:21 +0200

    First  3 test cases passed.

--- src/main/java/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,13 @@
 package kata.fizzbuzz;
 
 public class FizzBuzz {
-
+	public String[] getArrayAnswer(int lastNumber) {
+		String[] answer = new String[lastNumber];
+		if(lastNumber > 0) {
+			for(int i = 0; i < lastNumber; i++) {
+				answer[i] = Integer.toString(i+1);	
+			}
+		}
+		return answer;
+	}
 }
--- src/test/java/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/kata/fizzbuzz/FizzBuzzTest.java
@@ -1,13 +1,29 @@
 package kata.fizzbuzz;
 
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
 	
+	private FizzBuzz fizzBuzz;
+	
+	@Before
+	public void before() {
+		this.fizzBuzz = new FizzBuzz();
+	}
+	
 	@Test
-	public void doesThisWork() {
-		assertNotNull(new FizzBuzz());
+	public void fizzBuzzNotNull() {
+		assertNotNull(fizzBuzz);
+	}
+	
+	@Test
+	public void testFizzBuzz() {
+		assertArrayEquals(new String[0], fizzBuzz.getArrayAnswer(0));
+		assertArrayEquals(new String[]{"1"}, fizzBuzz.getArrayAnswer(1));
+		assertArrayEquals(new String[]{"1","2"}, fizzBuzz.getArrayAnswer(2));
 	}
 	
 }
